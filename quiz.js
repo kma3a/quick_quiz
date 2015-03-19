7c907ed6df4618ae97678b2a3e581c32895088ab
Kelsey Ando <kma3a@virginia.edu>
2015-03-19 09:27:55 -0400

    adding the function to decrease the score and some driver code

--- quiz.js
+++ quiz.js
@@ -11,7 +11,14 @@ ScoreKeeper.prototype.rightAnswer = function() {
 	this.currentScore += 10;
 };
 
+ScoreKeeper.prototype.wrongAnswer = function() {
+	this.currentScore -= 10;
+};
+
+
 
 var myScore = new ScoreKeeper;
 myScore.rightAnswer();
 console.log(myScore.currentScore === 10);
+myScore.wrongAnswer();
+console.log(myScore.currentScore === 0);
