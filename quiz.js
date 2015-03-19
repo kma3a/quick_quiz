7f7af0e192150a79e4f881f3eb6e9989bcbf23bd
Kelsey Ando <kma3a@virginia.edu>
2015-03-19 11:26:49 -0400

    adding fixes to the check input so it is reuseable and adding the creat question to the menu

--- quiz.js
+++ quiz.js
@@ -24,16 +24,17 @@ function Quiz(){
 }
 
 Quiz.prototype.createQuestion = function() {
-	var question = this.checkInputQuestion(	this.getInput("What would you like your question to be?"));
-	var answer  = this.checkInputAnswer( this.getInput("What would you like your question to be?"));
+	var question = this.checkInput("What would you like your question to be?");
+	var answer  = this.checkInput("What would you like your question to be?");
 	this.quizQuestions.push(new Questions(question, answer)); 
 }
 
-Quiz.prototype.checkInputQuestion = function(question) {
-	if (question === "") {
-		return this.checkInputQuestion(	this.getInput("What would you like your question to be?"));
+Quiz.prototype.checkInput = function(message) {
+	var input = this.getInput(message);
+	if (input  === "") {
+		return this.checkInputQuestion(message);
 	} else {
-		return question;
+		return input;
 	}
 }
 
@@ -50,6 +51,8 @@ Quiz.prototype.menu = function() {
 	console.log("1- create questions\n2- take quiz\n3- exit program");
 	switch(this.getInput("What would you like to do?")) {
 		case "1":
+			this.createQuestion();
+			this.menu();
 			break;
 		case "2":
 			break;
