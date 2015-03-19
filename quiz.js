2fbd125e7196e5b9fd850df3dfb069173be21abc
Kelsey Ando <kma3a@virginia.edu>
2015-03-19 17:14:53 -0400

    changing the switch statement into an object

--- quiz.js
+++ quiz.js
@@ -33,6 +33,19 @@ ScoreKeeper.prototype.wrongAnswer = function() {
 function Quiz(){
 	this.currentScore = new ScoreKeeper;
 	this.quizQuestions = [];
+	this.actions = {
+		"1": function() {
+			myQuiz.createQuestion();
+			myQuiz.menu();
+			},
+		"2": function() {
+			myQuiz.askQuestions();
+			myQuiz.menu();
+		},
+		"3": function() {
+			myQuiz.endGame();
+		}
+	};
 }
 
 Quiz.prototype.createQuestion = function() {
@@ -98,21 +111,13 @@ Quiz.prototype.endGame = function() {
 }
 
 Quiz.prototype.menu = function() {
+
 	console.log("1- create questions\n2- take quiz\n3- exit program");
-	switch(this.getInput("What would you like to do?")) {
-		case "1":
-			this.createQuestion();
-			this.menu();
-			break;
-		case "2":
-			this.askQuestions();
-			this.menu();
-			break;
-		case "3":
-			this.endGame();
-			break;
-		default:
-			this.menu();
+	var userInput = this.getInput("What would you like to do?");
+	if (this.actions[userInput]){
+		this.actions[userInput]();	
+	} else {
+		this.menu();
 	}
 }
 
